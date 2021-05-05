31219b944fcbe6d29e81f2e53e315dd6ab9d0aca
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-05-05 18:04:58 +0100

    Add form and post route to for user to enter a city of their choice + fix broken icon

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const https = require('https');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const weatherApiKey = process.env.WEATHER_API_KEY;
-const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${weatherApiKey}`;
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+  const weatherApiKey = process.env.WEATHER_API_KEY;
+  const query = req.body.cityName;
+  const unit = 'metric';
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${weatherApiKey}&units=${unit}"`;
+
   https.get(weatherApiUrl, function (response) {
     console.log(response.statusCode);
 
@@ -15,10 +24,13 @@ app.get('/', function (req, res) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const weatherDesc = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
       res.write(
-        `<h1>The temperature in London is ${temp} degrees Celcius.</h1>`
+        `<h1>The temperature in ${query} is ${temp} degrees Celcius.</h1>`
       );
       res.write(`<p>The weather is currently ${weatherDesc}.</p>`);
+      res.write('<img src=' + imageURL + '>');
       res.send();
     });
   });
